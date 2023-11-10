e43d9e3e6465485c0b07fbb8a466e933d4a1db1e
Jaqueline Donin <40238428+Jaquedonin@users.noreply.github.com>
2023-11-10 14:18:20 -0300

    Update App.js
    Alteração do nome da página

--- src/home/App.js
+++ src/home/App.js
@@ -3,15 +3,23 @@ import Footer2 from '../Componentes/footer2';
 import Header1 from '../Componentes/Header1';
 
 function App() {
-    return (
-        <div className="App">
-        <Navbar/>
-        <Header1/>
-        <Footer2/>
-        
-        </div>
-    );
-
+  const [pageTitle, setPageTitle] = useState('Votação Festival');
+  useEffect(() => {
+    document.title = pageTitle;
+  }, [pageTitle]);
+  const handleTitleChange = (event) => {
+    setPageTitle(event.target.value);
+  };
+  return (
+    <div>
+      <h1>Votação Festival</h1>
+      <input
+        type="text"
+        value={pageTitle}
+        onChange={handleTitleChange}
+        placeholder="Votação Festival"
+      />
+    </div>
+  );
 }
-
 export default App;
